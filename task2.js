293ccbb180af432ff1118fd3d0a2f75cc4f334a4
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-12 17:53:23 +0300

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,9 @@
+let employee = {
+    firstName: "Peter",			
+    lastName: "Peterson",
+    position: "developer",  		
+    experience : 4,
+    education: "higher"
+};
+console.log(Object.values(employee));
+console.log(Object.keys(employee));
